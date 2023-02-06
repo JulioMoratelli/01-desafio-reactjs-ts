0cf2c4f39944a7da8116eb901756a454a4472c69
Julio Moratelli <l3xpooh@gmail.com>
2023-02-06 11:18:13 -0300

    Header finalizado

--- src/componentes/Header.tsx
+++ src/componentes/Header.tsx
@@ -5,9 +5,7 @@ import styles from './Header.module.css'
 export function Header(){
     return(
         <header className={styles.header}>
-            <p><RocketLaunch size={40} /></p>
-            <p className={styles.titulo}>to</p>
-            <p className={styles.titulo}>do</p>
+            <p><RocketLaunch size={40} />todo</p>
         </header>
     )
 }
